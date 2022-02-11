2e334f519e3d8149b5e96acce6920643af5ba42c
Ahmad Raza <raza.ataki@gmail.com>
2022-02-11 11:32:59 +0500

    update welcome page

--- welcome.php
+++ welcome.php
@@ -8,15 +8,21 @@ include "User_class.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <nav>
-        <ul>
-            <li><a href="users.php">Users</a></li>
+    <div class="welcome_main">
+        <nav>
+            <ul>
+            <li><a href="users.php">View Users</a></li>
+            <li><a href="articles.php">View Articles</a></li>
             <li><a href="/">Add User</a></li>
+            <li><a href="add_article.php">Add Article</a></li>
 
         </ul>
     </nav>
     <h1>Welcome</h1>
+    <img class="welcome_image" src="images/welcome.jpg" alt="">
+</div>
 </body>
 </html>
